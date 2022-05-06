4c743a76c3a492cb6dbf199de230047ab7f9fcb8
bassemtarek <bassemtarek514@gmail.com>
2022-05-06 02:29:23 +0200

    Password & DuplicateEmail Validation

--- Api/Api/Controllers/AccountController.cs
+++ Api/Api/Controllers/AccountController.cs
@@ -97,6 +97,13 @@ namespace Api.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorRespone {Errors = new[]{
+                "Email Address is in use"}
+                });
+            }
+
             var user = new AppUser()
             {
                 DisplayName = registerDto.DisplayName,
