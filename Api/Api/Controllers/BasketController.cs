3696c3b8e064f14c981dcfe4d33575944aa90efc
bassemtarek <bassemtarek514@gmail.com>
2022-05-06 15:31:26 +0200

    Bascket quntity&price Validation

--- Api/Api/Controllers/BasketController.cs
+++ Api/Api/Controllers/BasketController.cs
@@ -3,14 +3,19 @@ using Core.Models;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Api.Controllers;
+using Api.DTOs;
+using AutoMapper;
 
 namespace API.Controllers
 {
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
@@ -23,9 +28,11 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBascketDto basket)
         {
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBascketDto, CustomerBasket>(basket);
+
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
 
             return Ok(updatedBasket);
         }
